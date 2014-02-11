bc7221d3bff3bc07f84e937c129ebb9caaa39051
Mathieu <mathieu.dou@gmail.com>
2014-02-11 14:08:24 +0100

    MAJ selon le mail de Mme Conan !

--- project_team.php
+++ project_team.php
@@ -9,7 +9,7 @@
                             </a>    
                             <div class="presentation">
                                 <h3> Simon LEMOINE </h3></br>
-                                   <p>I'm Simon, 20 years old, student in BS Degree of University in Valenciennes. I'm passionate and curious about new media and web technologies. I successfully obtained a DUT Services and Communication Networks at the IUT de Lens. I wanna be a Front-End Developper. I like to write splendid and smart code.</p>
+                                   <p>I'm Simon, 20 years old, student in Bsc T2I at the University of Valenciennes. I'm passionate and curious about new media and web technologies. I successfully obtained a DUT Services and Communication Networks, 2-years University diploma, at the IUT de Lens. I wanna be a Front-End Developper. I like writing splendid and smart codes.</p>
                             </div>                      
                         </div> 
                         <div class="member">
@@ -18,7 +18,7 @@
                             </a>    
                             <div class="presentation">
                                 <h3> Mathieu DOUHET </h3></br>
-                                  <p> My name is Mathieu, I'm 20 years old. After my DUT Services and Communication Networks, IUT de Lens. I went to a BS Degree in Information Technology and the Internet. I am passionate about the development, integration web. Curious in the field of new technologies</p>
+                                  <p> My name is Mathieu,20 years old and I am a student in Bsc T2I at the University of Valenciennes. I obtained a DUT Services and Communication Networks, 2-years University diploma, at the IUT de Lens. I'm fascinated by the Web development and the Web integration, but curious in the field of the new technologies.I wanna be a web integrator.</p>
                             </div>                      
                         </div>
                         <div class="member">
@@ -27,7 +27,7 @@
                             </a>    
                             <div class="presentation">
                                  <h3>Romain LEMICHEZ </h3></br>
-                                   <p>My name is Romain, passionate about the field of web and new technologies, I have the ambition to become a web developer. I am currently in BS Degree in Information Technology and the Internet of University in Valenciennes (FRANCE, 59)</p>
+                                   <p>My name is Romain, passionate about the web and new technologies, I have the ambition to become a web developer. I am currently in Bsc T2I at the University of Valenciennes (FRANCE, 59)</p>
                             </div>                      
                         </div>                                                  
                     </div>
@@ -37,7 +37,7 @@
             <div id="select">
                 <div class="description">
                     <h3 id="project">The project & context</h3>
-                    <p>We are 3 students, BS Degree in Information Technology and the Internet. This project is an open source project, So he's not                         sponsored by a business or for an professional agency. The main objective of this web site is going to be to propose a tool                           allowing to create a base CSS by simplifying the programming thanks to a simplistic interface and without touch the code CSS.</p>
+                    <p>We are 3 students, Bsc in Information Technology and the Internet. This project is an open source project, so he's not                         sponsored by a compagny or for a professional agency. The main objective of this web site is to offer a tool                           allowing to create a CSS basis by simplifying programming thanks to a simplistic interface and without touching the CSS code.</p>
                 </div>
                 <div class="pic right">
                     <img src="img/test_project.png" alt="project">
